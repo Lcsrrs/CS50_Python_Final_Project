1af9dba1adc757af1d16a2eac84f863b4862e4f3
Lucas Ramos <lcsramos.r@gmail.com>
2025-07-22 10:49:17 -0300

    Initial commit and project structure

---
+++ project.py
@@ -0,0 +1,29 @@
+import re
+
+def main():
+    ...
+
+def load_file(path):
+    ...
+
+def extract_messages(lines):
+    ...
+
+def count_user_messages(user):
+    ...
+
+def count_dai_messages(day):
+    ...
+
+def frequent_words(first_day, last_day):
+    ...
+
+def frequent_emojis(first_day, last_day):
+    ...
+
+def report():
+    ...
+
+if __name__ == "__main__":
+    main()
+
