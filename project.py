52fe0d515b12e7b9f94e992c9142c13eebe3de2d
Lucas Ramos <lcsramos.r@gmail.com>
2025-07-29 22:11:59 -0300

    added report function with graphics of messages per user per day and all time messages per user between given dates

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ import sys
 import time
 from emoji import UNICODE_EMOJI
 from collections import Counter
+import matplotlib.pyplot as plt
+
 
 
 
@@ -16,19 +18,20 @@ def main():
     message_per_user = count_user_messages({user: 0 for user in users}, messages)
     messages_in_day = count_day_messages(messages, "19/11/2024", "22/11/2024")
     frequent_words = count_frequent_words(messages, "01/01/2024", "31/12/2024")
-    index = 0
+    #index = 0
     # for word in sorted(frequent_words, reverse=True, key=lambda s: s["times"]):
     #     print(f"Word: {word["word"]}, apeared {word["times"]} times")
     #     index += 1
     #     if index > 10:
     #         break
-    frequent_emojis = count_frequent_emojis(messages, "01/01/2024", "31/12/2024")
-    print(frequent_emojis)
-    for emoji in sorted(frequent_emojis.items(), reverse=True, key=lambda kv: (kv[1], kv[0])):
-        print(f"{emoji[0]} appeared {emoji[1]} times")
-        index += 1
-        if index > 10:
-            break
+    # frequent_emojis = count_frequent_emojis(messages, "01/01/2024", "31/12/2024")
+    # print(frequent_emojis)
+    # for emoji in sorted(frequent_emojis.items(), reverse=True, key=lambda kv: (kv[1], kv[0])):
+    #     print(f"{emoji[0]} appeared {emoji[1]} times")
+    #     index += 1
+    #     if index > 10:
+    #         break
+    report(messages, users, "01/01/2023", "31/12/2025")
         
 def load_file(path):
     """ Load the .txt file extracted from WhatsApp """
@@ -97,6 +100,8 @@ def count_day_messages(messages, first_day, last_day = 0):
         
 
 def count_frequent_words(messages, first_day, last_day = 0):
+    """ Count the ocurrencys of every word between given dates """
+
     words = []
 
     first_day=time.strptime(str(first_day).replace("/", " "), "%d %m %Y")
@@ -107,25 +112,27 @@ def count_frequent_words(messages, first_day, last_day = 0):
         last_day = time.strptime(str(last_day).replace("/", " "), "%d %m %Y")
 
     for message in messages:
-        if match := re.search(r"^\d\d/\d\d/\d\d\d\d \d\d:\d\d - (?:.[^:]*): (.*)", message):
-            for word in match.group(1).split():
-                if word.lower() in ["<mídia", "oculta>", "e", "a", "de", "da", "do", "se", "que", "o", "pra", "para", "te", "é", "um", "ta", "na", "no", "as", "mas", "os", "com", "aí", "(arquivo", "anexado)", "ou"]:
-                    break
-                index = find_word(words, "word", word)
-                if index == -1:
-                    words.append({"word": word, "times": 1})
-                else:
-                    words[index]["times"] += 1
+        if match := re.search(r"^(\d\d)/(\d\d)/(\d\d\d\d) \d\d:\d\d - (?:.[^:]*): (.*)", message):
+            message_date = time.strptime(f"{match.group(1)} {match.group(2)} {match.group(3)}", "%d %m %Y")
+            if first_day <= message_date <= last_day:
+                for word in match.group(4).split():
+                    if word.lower() in ["<mídia", "oculta>", "e", "a", "de", "da", "do", "se", "que", "o", "pra", "para", "te", "é", "um", "ta", "na", "no", "as", "mas", "os", "com", "aí", "(arquivo", "anexado)", "ou"]:
+                        break
+                    index = find_word(words, "word", word)
+                    if index == -1:
+                        words.append({"word": word, "times": 1})
+                    else:
+                        words[index]["times"] += 1
         else:
-            for word in message.split():
-                if word.lower() in ["<mídia", "oculta>", "e", "a", "de", "da", "do", "se", "que", "o", "pra", "para", "te", "é", "um", "ta", "na", "no", "as", "mas", "os", "com", "aí", "(arquivo", "anexado)", "ou"]:
-                    break
-                index = find_word(words, "word", word)
-                if index == -1:
-                    words.append({"word": word, "times": 1})
-                else:
-                    words[index]["times"] += 1
-
+            if first_day <= message_date <= last_day:
+                for word in message.split():
+                    if word.lower() in ["<mídia", "oculta>", "e", "a", "de", "da", "do", "se", "que", "o", "pra", "para", "te", "é", "um", "ta", "na", "no", "as", "mas", "os", "com", "aí", "(arquivo", "anexado)", "ou"]:
+                        break
+                    index = find_word(words, "word", word)
+                    if index == -1:
+                        words.append({"word": word, "times": 1})
+                    else:
+                        words[index]["times"] += 1
     return words
 
 def find_word(lst, key, value):
@@ -146,21 +153,81 @@ def count_frequent_emojis(messages, first_day, last_day = 0):
         last_day = time.strptime(str(last_day).replace("/", " "), "%d %m %Y")
 
     for message in messages:
-        if match := re.search(r"^\d\d/\d\d/\d\d\d\d \d\d:\d\d - (?:.[^:]*): (.*)", message):
-            for word in match.group(1).split():
-                for character in word:
-                    if character in UNICODE_EMOJI["en"]:
-                        emoji.append(character)
+        if match := re.search(r"^(\d\d)/(\d\d)/(\d\d\d\d) \d\d:\d\d - (?:.[^:]*): (.*)", message):
+            message_date = time.strptime(f"{match.group(1)} {match.group(2)} {match.group(3)}", "%d %m %Y")
+            if first_day <= message_date <= last_day:
+                for word in match.group(4).split():
+                    for character in word:
+                        if character in UNICODE_EMOJI["en"]:
+                            emoji.append(character)
         else:
-            for word in message:
-                for character in word:
-                    if character in UNICODE_EMOJI["en"]:
-                        emoji.append(character)
+            if first_day <= message_date <= last_day:
+                for word in message:
+                    for character in word:
+                        if character in UNICODE_EMOJI["en"]:
+                            emoji.append(character)
 
     return Counter(emoji)
 
-def report():
-    ...
+def report(messages, users, first_day, last_day = 0):
+    tp_all_time_user_messages = {name: 0 for name in users}
+    day_users_messages = {name: 0 for name in users}
+    chat_days=[]
+    message_per_user = []
+    all_time_message_per_user = []
+    first_day=time.strptime(str(first_day).replace("/", " "), "%d %m %Y")
+
+    if last_day == 0:
+        last_day = first_day
+    else:
+        last_day = time.strptime(str(last_day).replace("/", " "), "%d %m %Y")
+
+    for message in messages:
+        if match := re.search(r"^(\d\d)/(\d\d)/(\d\d\d\d) \d\d:\d\d - (.[^:]*):", message):
+            message_date = time.strptime(f"{match.group(1)} {match.group(2)} {match.group(3)}", "%d %m %Y")
+            if first_day <= message_date <= last_day:
+                if f"{match.group(1)}/{match.group(2)}/{match.group(3)}" in chat_days:
+                    day_users_messages[match.group(4)] += 1
+                    tp_all_time_user_messages[match.group(4)] += 1
+                else:
+                    chat_days.append(f"{match.group(1)}/{match.group(2)}/{match.group(3)}")
+                    if len(chat_days) == 1:
+                        day_users_messages[match.group(4)] += 1
+                        tp_all_time_user_messages[match.group(4)] += 1
+                        continue
+                    temp_message = []
+                    for value in day_users_messages:
+                        temp_message.append(day_users_messages[value])
+                    message_per_user.append(temp_message)
+                    for value in day_users_messages:
+                        day_users_messages[value] = 0
+
+                    day_users_messages[match.group(4)] += 1
+                    tp_all_time_user_messages[match.group(4)] += 1
+
+    temp_message = []
+    for value in day_users_messages:
+        temp_message.append(day_users_messages[value])
+    message_per_user.append(temp_message)
+
+    for value in tp_all_time_user_messages:
+        all_time_message_per_user.append(tp_all_time_user_messages[value])
+
+    plt.plot(chat_days, message_per_user, label=users)
+    plt.xlabel('Day')
+    plt.ylabel('Number of messages')
+    plt.xticks(rotation=45)
+    plt.title('Messages per day per user')
+    plt.legend()
+    plt.show()
+
+    fig, ax = plt.subplots()
+    hbars = ax.barh(users, all_time_message_per_user, align='center')
+    ax.bar_label(hbars)
+    ax.set_xlabel('Messages per user')
+    plt.title('Messages per user')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
